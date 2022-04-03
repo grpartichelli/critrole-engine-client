1a844b1fc4c8ba9da10a4ce8fbb21c67d6026183
grpartichelli <gabriel.partichelli@aegro.com.br>
2022-04-03 16:59:42 -0300

    test

--- server.js
+++ server.js
@@ -1,6 +1,11 @@
 const express = require('express');
 const path = require('path');
 const app = express();
+const proxy = require('express-http-proxy');
+
+
+app.use('/api/*', proxy('https://critrole-engine-client.herokuapp.com/'));
+
 app.use(express.static(__dirname + '/dist/critrole-engine-client'));
 app.get('/*', function(req,res) {
   res.sendFile(path.join(__dirname+
