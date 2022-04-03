1b41a8a477dd5a8a157365057ccdbf1daa6bf150
grpartichelli <gabriel.partichelli@aegro.com.br>
2022-04-03 17:57:27 -0300

    fix

--- src/service/service.ts
+++ src/service/service.ts
@@ -1,10 +1,11 @@
 import {Injectable} from "@angular/core";
 import {HttpClient} from "@angular/common/http";
 import {Observable} from "rxjs";
+import {environment} from "../environments/environment";
 
 @Injectable({providedIn: 'root'})
 export class Service {
-  private readonly prod_url : string = '/api/';
+  private readonly prod_url : string = `${environment.api_url}/api/`;
 
   constructor(private  httpClient: HttpClient) {
   }
