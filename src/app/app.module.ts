215307f54703708d0d660195ac2f78578643ab93
grpartichelli <gabriel.partichelli@aegro.com.br>
2022-04-25 17:42:44 -0300

    feat: add routing and imports

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,17 +2,48 @@ import {NgModule} from '@angular/core';
 import {BrowserModule} from '@angular/platform-browser';
 
 import {AppComponent} from './app.component';
-import {HttpClientModule} from "@angular/common/http";
+import {BrowserAnimationsModule} from '@angular/platform-browser/animations';
+import {MatToolbarModule} from '@angular/material/toolbar';
+import {MatIconModule} from '@angular/material/icon';
+import {MatButtonModule} from '@angular/material/button';
+import {MatFormFieldModule} from '@angular/material/form-field';
+import {MatInputModule} from '@angular/material/input';
+import {MatCardModule} from '@angular/material/card';
+import {AppRoutingModule} from './app-routing.module';
+
+import {MatSidenavModule} from '@angular/material/sidenav';
+import {MatDividerModule} from '@angular/material/divider';
+import {MatListModule} from '@angular/material/list';
+import {FormsModule} from '@angular/forms';
+
+import {MatDialogModule} from '@angular/material/dialog';
+import {MatRadioModule} from '@angular/material/radio';
+import {CritroleSearchBarComponent} from "../critrole-home/critrole-search-bar/critrole-search-bar.component";
 import {CritroleHomeComponent} from "../critrole-home/critrole-home.component";
 
+
 @NgModule({
-    declarations: [
-        AppComponent,
-        CritroleHomeComponent
-    ],
+  declarations: [
+    AppComponent,
+    CritroleHomeComponent,
+    CritroleSearchBarComponent
+  ],
   imports: [
     BrowserModule,
-    HttpClientModule
+    BrowserAnimationsModule,
+    MatToolbarModule,
+    MatIconModule,
+    MatButtonModule,
+    MatFormFieldModule,
+    MatInputModule,
+    MatCardModule,
+    AppRoutingModule,
+    MatSidenavModule,
+    MatDividerModule,
+    MatListModule,
+    FormsModule,
+    MatDialogModule,
+    MatRadioModule,
   ],
   providers: [],
   bootstrap: [AppComponent]
