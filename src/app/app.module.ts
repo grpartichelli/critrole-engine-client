39a900323841148f2681daee7666468cfc5cc437
grpartichelli <gabriel.partichelli@aegro.com.br>
2022-04-25 18:03:15 -0300

    feat: fix the styling thing

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -6,6 +6,7 @@ import {BrowserAnimationsModule} from '@angular/platform-browser/animations';
 import {MatToolbarModule} from '@angular/material/toolbar';
 import {MatIconModule} from '@angular/material/icon';
 import {MatButtonModule} from '@angular/material/button';
+
 import {MatFormFieldModule} from '@angular/material/form-field';
 import {MatInputModule} from '@angular/material/input';
 import {MatCardModule} from '@angular/material/card';
@@ -14,6 +15,7 @@ import {AppRoutingModule} from './app-routing.module';
 import {MatSidenavModule} from '@angular/material/sidenav';
 import {MatDividerModule} from '@angular/material/divider';
 import {MatListModule} from '@angular/material/list';
+
 import {FormsModule} from '@angular/forms';
 
 import {MatDialogModule} from '@angular/material/dialog';
