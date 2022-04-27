5a51801dfe280d4e1e3240e8241917d6bb50d37f
grpartichelli <gabriel.partichelli@aegro.com.br>
2022-04-27 18:44:36 -0300

    feat: add calls for episode number and nickname

--- src/service/transcript-service.ts
+++ src/service/transcript-service.ts
@@ -1,20 +1,29 @@
 import {Injectable} from "@angular/core";
-import {HttpClient} from "@angular/common/http";
+import {HttpClient, HttpParams} from "@angular/common/http";
 import {environment} from "../environments/environment";
 import {TranscriptModel} from "../model/transcript.model";
 
 @Injectable({providedIn: 'root'})
 export class TranscriptService {
-  private readonly url : string = `${environment.api_url}/api/transcripts/`;
+  private readonly url: string = `${environment.api_url}/api/transcripts/`;
 
-  constructor(private  httpClient: HttpClient) {
+  constructor(private httpClient: HttpClient) {
   }
 
-  public findByText(text : string) : Promise<Array<TranscriptModel> | undefined>  {
+  public findByText(text: string, episode?: number, nickname?: string): Promise<Array<TranscriptModel> | undefined> {
     if (text === '' || !text) {
-        return Promise.reject()
+      return Promise.reject()
+    }
+    let params = new HttpParams();
+    if (episode) {
+      params = params.set('episode_number', episode)
     }
 
-     return this.httpClient.get<Array<TranscriptModel>>(`${this.url}search/${text}`).toPromise()
+    if (nickname) {
+      params = params.set('actor_nickname', nickname)
+    }
+    return this.httpClient.get<Array<TranscriptModel>>(`${this.url}search/${text}`, {params}).toPromise()
   }
+
+
 }
